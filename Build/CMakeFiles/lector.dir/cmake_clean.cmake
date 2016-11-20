c78bdbad1b1d3e285d18052852c2c757173d7f1b
Jose Quintero <joseantonioq9@gmail.com>
2016-11-20 16:07:28 -0500

    opencv

---
+++ Build/CMakeFiles/lector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lector.dir/lector.cpp.o"
+  "lector.pdb"
+  "lector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lector.cpp
+++ lector.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 using namespace cv;
 int main (int argc,char *argv[])
 {
 	if (argc<2)
 	{
 		std::cout<<"Numero de argumentos invalido"<<std::endl;
-		exit(1);
+		return 1;
 	}
 	VideoCapture video (argv[1]);
 	
