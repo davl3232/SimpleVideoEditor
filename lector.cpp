9b05afc3d6a39b0107265f255120ca97decdb0a3
davl3232 <davl3232@gmail.com>
2016-11-20 16:32:11 -0500

    Leer y mostrar video pasado por parámetro.

--- lector.cpp
+++ lector.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 using namespace cv;
-int main (int argc,char *argv[])
+
+#define NOMBRE_VENTANA "Simple Video Editor"
+
+int main (int argc, char *argv[])
 {
-	if (argc<2)
+	if (argc < 2)
 	{
-		std::cout<<"Numero de argumentos invalido"<<std::endl;
+		std::cout << "Numero de argumentos invalido." << std::endl;
 		return 1;
 	}
-	VideoCapture video (argv[1]);
-	
+
+	VideoCapture video;
+	Mat frame;
+
+	// Intentar abrir video
+	if (!video.open(argv[1]))
+	{
+		std::cout << "Video no encontrado." << std::endl;
+		return 1;
+	}
+
+	// Crear ventana
+	namedWindow( NOMBRE_VENTANA, CV_WINDOW_AUTOSIZE );
+	while (1) {
+		// Extraer frame
+		video >> frame;
+		
+		if ( !frame.empty() ) {
+			// Mostrar imagen en ventana
+			imshow(NOMBRE_VENTANA, frame);
+			if ( waitKey(1) == 27 ) break;
+		}
+	}
+	return 0;
 }
