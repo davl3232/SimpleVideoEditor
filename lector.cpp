03eef33ba0a71c47481a398f1204b4dd5eabdc7f
davl3232 <davl3232@gmail.com>
2016-11-21 11:08:14 -0500

    con temporizador y arreglando estado de mouse

--- lector.cpp
+++ lector.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <unistd.h>
+#include <ctime>
+#include <algorithm>
+#include <chrono>
 #include <opencv2/opencv.hpp>
 using namespace cv;
 using namespace std;
@@ -9,16 +12,14 @@ using namespace std;
 void ActualizarBarra( int, void* );
 void mouseHandler(int event, int x, int y, int flags, void* userdata);
 
-int estadoControles = 0;
+bool mouseOprimido = 0;
 bool reproduciendo = false;
-bool oprimiendo = false;
-Rect boton;
+bool oprimiendoPlay = false, oprimiendoStop = false;
+bool sobrePlay = false, sobreStop = false;
+Rect botonPlayArea, botonStopArea, videoArea;
 Mat canvas;
-
-int main (int argc, char *argv[])
-{
-	if (argc < 2)
-	{
+int main (int argc, char *argv[]) {
+	if (argc < 2) {
 		cout << "Numero de argumentos invalido." << endl;
 		return 1;
 	}
@@ -30,42 +31,86 @@ int main (int argc, char *argv[])
 	}
 
 	Mat frame;
-	double FPS = 24.0;
+	// Leer video a 24 fps
+	double fps = cap.get(CV_CAP_PROP_FPS);
 	string nombrePlay = "";
 	nombrePlay += "|>";
 	namedWindow(NOMBRE_VENTANA, CV_WINDOW_AUTOSIZE);
 
 	//setMouseCallback(const string& winname, MouseCallback onMouse, void* userdata=0 )
 
-	// Leer video a 24 FPS
-	while (true) {
-		cap >> frame;
-		if (frame.empty()) {
+	// Cargar GUI
+	Mat botonPlayOprimido = imread("../assets/play_pressed.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonPlayNormal = imread("../assets/play_normal.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonPlayHover = imread("../assets/play_hover.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonPauseOprimido = imread("../assets/pause_pressed.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonPauseNormal = imread("../assets/pause_normal.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonPauseHover = imread("../assets/pause_hover.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonStopOprimido = imread("../assets/stop_pressed.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonStopNormal = imread("../assets/stop_normal.png", CV_LOAD_IMAGE_COLOR);
+	Mat botonStopHover = imread("../assets/stop_hover.png", CV_LOAD_IMAGE_COLOR);
 
+	bool primeraIteracion = true;
+	int numFrames = cap.get(CV_CAP_PROP_FRAME_COUNT);
+	cout << numFrames << endl;
+	while (true) {
+	    auto t1 = chrono::high_resolution_clock::now();
+		cap >> frame;
+		if (primeraIteracion) {
+			videoArea = Rect(0, 0, frame.cols, frame.rows);
+			botonPlayArea = Rect(-botonPlayNormal.cols/2.0 + frame.cols/2.0, frame.rows, botonPlayNormal.cols, botonPlayNormal.rows);
+			botonStopArea = Rect(-botonPlayNormal.cols/2.0 + frame.cols/2.0 - botonStopNormal.cols*1.5, frame.rows, botonPlayNormal.cols, botonPlayNormal.rows);
+			primeraIteracion = false;
+		}
+		if (frame.empty()) {
 			cout << "Archivo terminado." << endl;
 			break;
 		}
-		
-		// Your boton
-		boton = Rect(0, 0, frame.cols, 50);
 
-		// The canvas
-		canvas = Mat(frame.rows + boton.height, frame.cols, frame.type(), Scalar(0.0, 0.0, 0.0));
+		// Canvas
+		canvas = Mat(frame.rows + botonPlayNormal.rows, frame.cols, frame.type(), Scalar(255,255,255,0));
 
-		// Draw the boton
-		canvas(boton) = Scalar(200, 200, 200);
-		canvas(boton);
-		bool reproduciendo = false;
-		putText(canvas, nombrePlay, Point(boton.width * 0.35, boton.height * 0.7), FONT_HERSHEY_PLAIN, 1, Scalar(255, 255, 255));
+		// Dibujar GUI
+		/*
 		if (oprimiendo) {
 			rectangle(canvas, boton, Scalar(200, 200, 200), 2);
 		} else {
 			rectangle(canvas, boton, Scalar(0, 0, 255), 2);
 		}
+		*/
+		// Dibujar GUI
 
-		// Draw the image
-		frame.copyTo(canvas(Rect(0, boton.height, frame.cols, frame.rows)));
-
+		if (reproduciendo) {
+			if (sobrePlay) {
+				if (oprimiendoPlay) {
+					botonPlayOprimido.copyTo(canvas(botonPlayArea));
+				} else {
+					botonPlayHover.copyTo(canvas(botonPlayArea));
+				}
+			} else {
+				botonPlayNormal.copyTo(canvas(botonPlayArea));
+			}
+		} else {
+			if (sobrePlay) {
+				if (oprimiendoPlay) {
+					botonPauseOprimido.copyTo(canvas(botonPlayArea));
+				} else {
+					botonPauseHover.copyTo(canvas(botonPlayArea));
+				}
+			} else {
+				botonPauseNormal.copyTo(canvas(botonPlayArea));
+			}
+		}
+		if (sobreStop) {
+			if (oprimiendoStop) {
+				botonStopOprimido.copyTo(canvas(botonStopArea));
+			} else {
+				botonStopHover.copyTo(canvas(botonStopArea));
+			}
+		} else {
+			botonStopNormal.copyTo(canvas(botonStopArea));
+		}
+		frame.copyTo(canvas(videoArea));
 		// Setup callback function
 		namedWindow(NOMBRE_VENTANA);
 		setMouseCallback(NOMBRE_VENTANA, mouseHandler);
@@ -73,31 +118,64 @@ int main (int argc, char *argv[])
 		imshow(NOMBRE_VENTANA, canvas);
 		reproduciendo = true;
 
+	    auto t2 = chrono::high_resolution_clock::now();
+	    chrono::duration<double> diff = t2 - t1;
+	    //std::cout << (double)chrono::duration_cast<chrono::nanoseconds>(diff).count()/1000000 << " ";
 		// Detener al oprimir ESC
-		if (waitKey(1000.0 / FPS) == 27) break;
+		if (waitKey(1000.0 / fps) == 27) break;
+		t2 = chrono::high_resolution_clock::now();
+	    diff = t2 - t1;
+	    //std::cout << (double)chrono::duration_cast<chrono::nanoseconds>(diff).count()/1000000 << '\n';
 	}
 
 	return 0;
 }
-
+/*
+	0: mouseSuelto ->
+	1: mouseOprimido
+*/
 // Callback del Mouse
-void mouseHandler(int event, int x, int y, int flags, void* userdata)
-{
-	if (event == EVENT_LBUTTONDOWN)
-	{
-		if (boton.contains(Point(x, y)))
-		{
-			cout << "Clicked!" << endl;
-			oprimiendo = true;
+void mouseHandler(int event, int x, int y, int flags, void* userdata) {
+	if (mouseOprimido) {
+		if (event == EVENT_LBUTTONUP) {
+			mouseOprimido = false;
+			oprimiendoPlay = false;
+			oprimiendoStop = false;
+			if (botonPlayArea.contains(Point(x, y))) {
+				sobrePlay = true;
+			} else {
+				sobrePlay = false;
+			}
+			if (botonStopArea.contains(Point(x, y))) {
+				sobreStop = true;
+			} else {
+				sobreStop = false;
+			}
+		}
+	} else {
+		if (event == EVENT_LBUTTONDOWN) {
+			mouseOprimido = true;
+			if (botonPlayArea.contains(Point(x, y))) {
+				oprimiendoPlay = true;
+			}
+			if (botonStopArea.contains(Point(x, y))) {
+				oprimiendoStop = true;
+			}
+			sobrePlay = false;
+			sobreStop = false;
+		} else if (event == EVENT_LBUTTONUP) {
+			if (botonPlayArea.contains(Point(x, y))) {
+				sobrePlay = true;
+			} else {
+				sobrePlay = false;
+			}
+			if (botonStopArea.contains(Point(x, y))) {
+				sobreStop = true;
+			} else {
+				sobreStop = false;
+			}
 		}
 	}
-	if (event == EVENT_LBUTTONUP)
-	{
-		oprimiendo = false;
-	}
-
-	imshow(NOMBRE_VENTANA, canvas);
-	waitKey(1);
 }
 /*
 static void onMouse( int event, int x, int y, int, void* )
